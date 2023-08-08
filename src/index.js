6cc8ba7a3c8601a85b8e22157caa1a41eae1caa5
owen <owen.chosen@gmail.com>
2023-08-08 13:41:09 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const init = () => {
-  
+    const inputForm = document.getElementsByTagName ("form")[0];
+    inputForm.addEventListener ('submit', (event) => {
+        event.preventDefault();
+        const inputValue = event.target.searchByID.value;
+        if (!isNaN(inputValue)) {
+            fetch(`http://localhost:3000/movies/${inputValue}`)
+            .then(response => {
+                if (!response.ok) throw new Error("Not Found");
+                return response.json();
+            })
+            .then(data => {
+                document.querySelector ("#movieDetails h4").textContent = data.title;
+                document.querySelector ("#movieDetails p").textContent = data.summary;
+            })
+            
+            .catch (error => {
+                document.querySelector ("#movieDetails h4").textContent = error.message;
+                document.querySelector ("#movieDetails p").textContent = "";
+
+            });
+        }
+        else {
+            document.querySelector ("#movieDetails h4").textContent = "Enter a Valid Movie ID";
+            document.querySelector ("#movieDetails p").textContent = "";
+        }
+        inputForm.reset();
+    });
+      
 }
 
 document.addEventListener('DOMContentLoaded', init);
